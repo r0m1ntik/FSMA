ea2c30d02db8e457a4afd78a8720aa962542fabf
r0m1ntik <r.badanin94@yandex.ru>
2023-02-28 21:28:16 +0100

    Minimalisation des requirements, affin d'evité les erreurs lors de la compilation chez les autres personnes.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,4 @@
 module com.fsma {
     requires javafx.fxml;
     requires jade;
-    requires java.desktop;
-    requires org.jetbrains.annotations;
-
-    opens View to javafx.fxml;
-    exports View;
 }
